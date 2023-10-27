12f90ba3ea41494103599d2fcf99f36b8fa02a4f
sanhue903 <s.sanhueza1001@gmail.com>
2023-10-27 12:55:15 -0300

    se arreglaron errores de la app

--- backend/api/config.py
+++ backend/api/config.py
@@ -2,7 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
--- backend/api/src/main/routes.py
+++ backend/api/src/main/routes.py
@@ -1,7 +1,6 @@
 from flask import render_template
 from src.main import bp
 
-
 @bp.route('/')
 def index():
     return render_template('index.html')
