0e877adabad6d35b85bacb80fbae4fb9847777c2
Lula Galvez <luisgalvezuk@gmail.com>
2023-11-01 19:17:40 -0300

    agregado backref a usuario para entrytime

--- backend/api/src/models/entrytime.py
+++ backend/api/src/models/entrytime.py
@@ -5,11 +5,10 @@ import datetime
 class EntryTime(db.Model):
     __tablename__ = 'entrytime'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
-    date_time = db.Column(db.DateTime,default=datetime.datetime.now(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref='entry_times')
+    date_time = db.Column(db.DateTime, default=datetime.datetime.now(), nullable=False)
     exit_time = db.relationship('ExitTime', uselist=False, backref='entrytime')
 
     def __repr__(self):
         return f'<EntryTime {self.id}>'
-    
-    
