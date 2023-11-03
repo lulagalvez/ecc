2716c9c39b56d8070d297a8d8aab431b3d51ac19
Lula Galvez <luisgalvezuk@gmail.com>
2023-11-02 23:05:51 -0300

    Cambiado de utcnow a datetime.now

--- backend/api/src/models/exittime.py
+++ backend/api/src/models/exittime.py
@@ -1,12 +1,12 @@
 from src.extensions import db
-from datetime import datetime
+import datetime
 
 class ExitTime(db.Model):
     __tablename__ = 'exittime'
     id = db.Column(db.Integer, primary_key=True)
     entry_time_id = db.Column(db.Integer, db.ForeignKey('entrytime.id'), unique=True)
     entry_time = db.relationship('EntryTime', back_populates='exit_time')
-    date_time = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    date_time = db.Column(db.DateTime, default=datetime.datetime.now(), nullable=False)
 
     def __repr__(self):
         return f'<ExitTime {self.id}>'
