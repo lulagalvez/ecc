a9231c7f1dd5fc8025014f18b1fa24a8152e14fc
Jose Toledo <jtoledo2019@inf.udec.cl>
2023-11-09 21:28:18 -0300

    feat(Web Menu)

--- frontend/Plataforma_Web/src/globalComponent/components/NavBar.jsx
+++ frontend/Plataforma_Web/src/globalComponent/components/NavBar.jsx
@@ -1,24 +1,31 @@
-
 import "../styles/NavBar.css";
 import Logo from "../../image/shield-image.png";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 import { Link } from "react-router-dom";
-import { Container, Image, Nav, Navbar, NavDropdown ,Form,Button} from "react-bootstrap";
-import { FaBell, FaUser, FaCog, FaSignOutAlt } from 'react-icons/fa'; // Importa los íconos de FontAwesome
+import {
+  Container,
+  Image,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  Button,
+} from "react-bootstrap";
+import { FaBell, FaUser, FaCog, FaSignOutAlt } from "react-icons/fa"; // Importa los íconos de FontAwesome
 
 const CircleWithLetter = styled.div`
   background-color: red;
-  width: 1vh;
-  height: 1vh;
+  width: 1em;
+  height: 1em;
   border-radius: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
   color: white;
-  font-size: 1.25vw;
-  margin-left: 2vh;
-  padding: 3vh;
+  font-size: 1.25em;
+  margin-left: 2em;
+  padding: 1.25em;
 `;
 
 const CustomNavDropdown = styled(NavDropdown)`
@@ -28,71 +35,95 @@ const CustomNavDropdown = styled(NavDropdown)`
 `;
 
 function CircleIcon({ letter }) {
-  return (
-    <CircleWithLetter>
-      {letter}
-    </CircleWithLetter>
-  );
+  return <CircleWithLetter>{letter}</CircleWithLetter>;
 }
 
 const NavBar = ({ usuario }) => {
   return (
-      <Navbar fluid as={Container} className="barra-navegacion" variant="dark" fixed="top" expand="md" bg="dark" data-bs-theme="dark">
-        <Navbar.Brand  as={Link} to={"/menupage"}>
-          <Image src={Logo} alt="Logo" style={{ maxWidth: "100%", maxHeight:"%100%" }} className="logo-baner ms-4 me-4"></Image>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
+    <Navbar
+      fluid
+      as={Container}
+      className="barra-navegacion"
+      variant="dark"
+      fixed="top"
+      expand="lg"
+      bg="dark"
+      data-bs-theme="dark"
+    >
+      <Navbar.Brand as={Link} to={"/menupage"}>
+        <Image
+          src={Logo}
+          alt="Logo"
+          style={{ maxWidth: "100%", maxHeight: "%100%" }}
+          className="logo-baner ms-4 me-4"
+        ></Image>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto ms-4 menus">
-            <Nav.Link className="me-2" as={Link} to="/bitacoras">Bitácora</Nav.Link>
-            <Nav.Link as={Link} to="/registro-horas">Historial</Nav.Link>
-            <Nav.Link as={Link} to="/registrar-usuario">Crear Usuario</Nav.Link>
-          </Nav>
-          <Nav className="me-auto">
-            <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Matricula"
-                    className="me-2"
-                    aria-label="Search"           
-                  />
-                  <Button variant="outline-success">Buscar</Button>
-                </Form>
-          </Nav>
-          <Nav>
-            <NavDropdown title={<FaBell className="icono-notificaciones" />} id="collapsible-nav-dropdown" className="ms-4 me-4 mt-2" >
-              <NavDropdown.Item href="#action/3.1" className="icono-con-texto">
-                <FaBell style={{ marginRight: '0.5rem' }}/> Notificaciones
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2" className="icono-con-texto">
-                <FaBell style={{ marginRight: '0.5rem' }}/> Notificación 1
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3" className="icono-con-texto">
-                <FaBell style={{ marginRight: '0.5rem' }}/> Notificación 2
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4" className="icono-con-texto">
-                <FaBell style={{ marginRight: '0.5rem' }}/> Notificación 3
-              </NavDropdown.Item>
-            </NavDropdown>
+          <Nav.Link className="me-2" as={Link} to="/bitacoras">
+            Bitácora
+          </Nav.Link>
+          <Nav.Link className="me-2" as={Link} to="/registro-horas">
+            Historial
+          </Nav.Link>
+          <Nav.Link className="me-2" as={Link} to="/registrar-usuario">
+            Crear Usuario
+          </Nav.Link>
+        </Nav>
+        <Nav className="me-auto">
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Matricula"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Buscar</Button>
+          </Form>
+        </Nav>
+        <Nav>
+          <NavDropdown
+            title={<FaBell className="icono-notificaciones" />}
+            id="collapsible-nav-dropdown"
+            className="ms-4 me-4 mt-2"
+          >
+            <NavDropdown.Item href="#action/3.1" className="icono-con-texto">
+              <FaBell style={{ marginRight: "0.5rem" }} /> Notificaciones
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.2" className="icono-con-texto">
+              <FaBell style={{ marginRight: "0.5rem" }} /> Notificación 1
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3" className="icono-con-texto">
+              <FaBell style={{ marginRight: "0.5rem" }} /> Notificación 2
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4" className="icono-con-texto">
+              <FaBell style={{ marginRight: "0.5rem" }} /> Notificación 3
+            </NavDropdown.Item>
+          </NavDropdown>
 
-            <CustomNavDropdown title={<CircleIcon letter="J" />} id="collapsible-nav-dropdown" className="me-5">
-              <NavDropdown.Item href="#action/3.1" className="icono-con-texto">
-                <FaUser style={{ marginRight: '0.5rem' }}/> Jose Toledo
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.3" className="icono-con-texto">
-                <FaCog  style={{ marginRight: '0.5rem' }} /> Ajustes
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4" className="icono-con-texto">
-  <FaSignOutAlt style={{ marginRight: '0.5rem' }} />
-  Cerrar sesión
-</NavDropdown.Item>
-            </CustomNavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+          <CustomNavDropdown
+            title={<CircleIcon letter="J" />}
+            id="collapsible-nav-dropdown"
+            className="me-5"
+          >
+            <NavDropdown.Item href="#action/3.1" className="icono-con-texto">
+              <FaUser style={{ marginRight: "0.5rem" }} /> Jose Toledo
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.3" className="icono-con-texto">
+              <FaCog style={{ marginRight: "0.5rem" }} /> Ajustes
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4" className="icono-con-texto">
+              <FaSignOutAlt style={{ marginRight: "0.5rem" }} />
+              Cerrar sesión
+            </NavDropdown.Item>
+          </CustomNavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
--- frontend/Plataforma_Web/src/views/MenuPage.jsx
+++ frontend/Plataforma_Web/src/views/MenuPage.jsx
@@ -1,34 +1,223 @@
-import { Container, Row, Col } from "react-bootstrap";
-import { motion } from "framer-motion";
+import React, { useState } from "react";
 
+import { Container, Row, Col, Pagination } from "react-bootstrap";
+import { motion } from "framer-motion";
 import NavBar from "../globalComponent/components/NavBar.jsx";
 import Cards from "../globalComponent/components/Cards.jsx";
-
 import "./style/MenuPage.css";
 
 const Menupage = () => {
-  // Hardcodeado , aca se consultara desde la api la informacion 
+  const pageSize = 12; // Cantidad de Cards por página
+  const [currentPage, setCurrentPage] = useState(1);
+
   const cardData = [
-    { id: 1, estado: "DISPONIBLE", ocupacion: "Ayudante" , nombre:"Jose Toledo Arcic" },
-    { id: 2, estado: "AUSENTE", ocupacion: "Conductor" , nombre:"Jose Toledo Arcic" },
-    { id: 3, estado: "DISPONIBLE", ocupacion: "Ayudante" , nombre:"Jose Toledo Arcic" },
-    { id: 4, estado: "AUSENTE", ocupacion: "Conductor" , nombre:"Jose Toledo Arcic" },
-    { id: 5, estado: "DISPONIBLE", ocupacion: "Conductor" , nombre:"Jose Toledo Arcic" },
-    { id: 6, estado: "DISPONIBLE", ocupacion: "Ayudante" , nombre:"Jose Toledo Arcic" },
-    { id: 7, estado: "AUSENTE", ocupacion: "Conductor" , nombre:"Jose Toledo Arcic" },
-    { id: 8, estado: "EMERGENCIA", ocupacion: "Conductor" , nombre:"Jose Toledo Arcic" },
-    { id: 9, estado: "EMERGENCIA", ocupacion: "Conductor" , nombre:"Jose Toledo Arcic" },
-    { id: 10, estado: "AUSENTE", ocupacion: "Conductor" , nombre:"Jose Toledo Arcic" },
-    // Agrega datos similares para las otras Cards
+    {
+      id: 1,
+      estado: "DISPONIBLE",
+      ocupacion: "Capitán de Bomberos",
+      nombre: "Carlos Rodríguez",
+    },
+    {
+      id: 2,
+      estado: "AUSENTE",
+      ocupacion: "Bombero Voluntario",
+      nombre: "Alejandra Gómez",
+    },
+    {
+      id: 3,
+      estado: "DISPONIBLE",
+      ocupacion: "Sargento de Bomberos",
+      nombre: "Roberto Mendoza",
+    },
+    {
+      id: 4,
+      estado: "AUSENTE",
+      ocupacion: "Bombero Rescatista",
+      nombre: "Laura Fernández",
+    },
+    {
+      id: 5,
+      estado: "DISPONIBLE",
+      ocupacion: "Teniente de Bomberos",
+      nombre: "Eduardo Castillo",
+    },
+    {
+      id: 6,
+      estado: "DISPONIBLE",
+      ocupacion: "Bombero Paramédico",
+      nombre: "Ana Pérez",
+    },
+    {
+      id: 7,
+      estado: "AUSENTE",
+      ocupacion: "Capitana de Bomberos",
+      nombre: "Martín González",
+    },
+    {
+      id: 8,
+      estado: "EMERGENCIA",
+      ocupacion: "Bombero de Rescate Acuático",
+      nombre: "María Ruiz",
+    },
+    {
+      id: 9,
+      estado: "EMERGENCIA",
+      ocupacion: "Bombero Especialista",
+      nombre: "Javier López",
+    },
+    {
+      id: 10,
+      estado: "AUSENTE",
+      ocupacion: "Bombero Técnico",
+      nombre: "Silvia Torres",
+    },
+    {
+      id: 11,
+      estado: "DISPONIBLE",
+      ocupacion: "Teniente de Bomberos",
+      nombre: "Luisa Morales",
+    },
+    {
+      id: 12,
+      estado: "AUSENTE",
+      ocupacion: "Bombero Rescatista",
+      nombre: "Ricardo Sánchez",
+    },
+    {
+      id: 13,
+      estado: "DISPONIBLE",
+      ocupacion: "Capitana de Bomberos",
+      nombre: "Pedro Navarro",
+    },
+    {
+      id: 14,
+      estado: "AUSENTE",
+      ocupacion: "Bombero Paramédico",
+      nombre: "Isabel García",
+    },
+    {
+      id: 15,
+      estado: "DISPONIBLE",
+      ocupacion: "Sargento de Bomberos",
+      nombre: "Juan Torres",
+    },
+    {
+      id: 16,
+      estado: "DISPONIBLE",
+      ocupacion: "Bombero de Rescate Acuático",
+      nombre: "Sofía Ramírez",
+    },
+    {
+      id: 17,
+      estado: "AUSENTE",
+      ocupacion: "Bombero Voluntario",
+      nombre: "Mateo Díaz",
+    },
+    {
+      id: 18,
+      estado: "EMERGENCIA",
+      ocupacion: "Bombero Especialista en Incendios Forestales",
+      nombre: "Laura Gómez",
+    },
+    {
+      id: 19,
+      estado: "EMERGENCIA",
+      ocupacion: "Bombero Técnico en Emergencias Médicas",
+      nombre: "Hugo Mendoza",
+    },
+    {
+      id: 20,
+      estado: "AUSENTE",
+      ocupacion: "Capitán de Bomberos",
+      nombre: "Camila López",
+    },
+    {
+      id: 21,
+      estado: "DISPONIBLE",
+      ocupacion: "Bombero Rescatista",
+      nombre: "Diego Fernández",
+    },
+    {
+      id: 22,
+      estado: "AUSENTE",
+      ocupacion: "Teniente de Bomberos",
+      nombre: "Carolina Pérez",
+    },
+    {
+      id: 23,
+      estado: "DISPONIBLE",
+      ocupacion: "Bombero Paramédico",
+      nombre: "Miguel Rodríguez",
+    },
+    {
+      id: 24,
+      estado: "DISPONIBLE",
+      ocupacion: "Capitana de Bomberos",
+      nombre: "Adriana Soto",
+    },
+    {
+      id: 25,
+      estado: "AUSENTE",
+      ocupacion: "Sargento de Bomberos",
+      nombre: "Javier González",
+    },
+    {
+      id: 26,
+      estado: "EMERGENCIA",
+      ocupacion: "Bombero de Rescate Acuático",
+      nombre: "Patricia Torres",
+    },
+    {
+      id: 27,
+      estado: "EMERGENCIA",
+      ocupacion: "Bombero de Incendios Forestales",
+      nombre: "Andrés Ramírez",
+    },
+    {
+      id: 28,
+      estado: "AUSENTE",
+      ocupacion: "Bombero Técnico",
+      nombre: "María Fernández",
+    },
+    {
+      id: 29,
+      estado: "DISPONIBLE",
+      ocupacion: "Teniente de Bomberos",
+      nombre: "Roberto Díaz",
+    },
+    {
+      id: 30,
+      estado: "AUSENTE",
+      ocupacion: "Bombero Rescatista",
+      nombre: "Ana Navarro",
+    },
+    // Agrega más datos similares para las otras Cards
   ];
+
+  // Calcular la cantidad total de páginas
+  const totalPages = Math.ceil(cardData.length / pageSize);
+
+  // Función para calcular el índice inicial y final de las Cards a mostrar en la página actual
+  const startIndex = (currentPage - 1) * pageSize;
+  const endIndex = Math.min(startIndex + pageSize, cardData.length);
+  const currentCards = cardData.slice(startIndex, endIndex);
 
   // Función para renderizar las Cards
   const renderCards = () => {
-    return cardData.map((card) => (
-      <Col key={card.id} xs={12} md={4} lg={2} className="my-4 ">
-        <Cards estado={card.estado} ocupacion={card.ocupacion} nombre={card.nombre} />
+    return currentCards.map((card) => (
+      <Col key={card.id} xs={12} md={4} lg={2} className="my-4">
+        <Cards
+          estado={card.estado}
+          ocupacion={card.ocupacion}
+          nombre={card.nombre}
+        />
       </Col>
     ));
+  };
+
+  // Función para manejar el cambio de página
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
   };
 
   return (
@@ -41,14 +230,27 @@ const Menupage = () => {
       <Container fluid className="pagina">
         <Row className="d-flex">
           <Col xs={12} md={12} className="p-0 col-1 my-5">
-            <NavBar></NavBar>
+            <NavBar />
           </Col>
-          <Col xs={12} md={12} className="col-2">
-            <Row>{renderCards()}</Row>
+          <Col
+            xs={12}
+            md={12}
+            className="col-2 d-flex flex-column align-items-center justify-content-center"
+          >
+            <Row className="mb-3">{renderCards()}</Row>
+            <Pagination size="md">
+              {[...Array(totalPages)].map((_, index) => (
+                <Pagination.Item
+                  key={index + 1}
+                  active={index + 1 === currentPage}
+                  onClick={() => handlePageChange(index + 1)}
+                >
+                  {index + 1}
+                </Pagination.Item>
+              ))}
+            </Pagination>
           </Col>
-          <Col xs={12} md={12} className="p-0 col-3">
-            {/* Agrega tu contenido en la tercera columna si es necesario */}
-          </Col>
+          <Col xs={12} md={12} className="p-0 col-3"></Col>
         </Row>
       </Container>
     </motion.div>
@@ -56,4 +258,3 @@ const Menupage = () => {
 };
 
 export default Menupage;
-
